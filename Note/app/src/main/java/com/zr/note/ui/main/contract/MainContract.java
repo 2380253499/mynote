d7338a8e0a1ef078eb880a592478dec5d30b44eb
zhongrui <2380253499@qq.com>
2016-09-09 18:36:59 +0800

    mvp增加contract

---
+++ Note/app/src/main/java/com/zr/note/ui/main/contract/MainContract.java
@@ -0,0 +1,17 @@
+package com.zr.note.ui.main.contract;
+
+import com.zr.note.base.BaseView;
+import com.zr.note.base.IPresenter;
+
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+public interface MainContract {
+    interface View extends BaseView{
+
+    }
+    interface Presenter extends IPresenter<View> {
+
+    }
+
+}
