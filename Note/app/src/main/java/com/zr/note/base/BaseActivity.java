edebc901e03e0554b11502436e2249e8dd131297
zhongrui <2380253499@qq.com>
2016-10-13 18:04:02 +0800

    页面设计

--- Note/app/src/main/java/com/zr/note/base/BaseActivity.java
+++ Note/app/src/main/java/com/zr/note/base/BaseActivity.java
@@ -83,6 +83,9 @@ public abstract class BaseActivity<V extends BaseView, P extends BasePresenter<V
     private void setColorPrimaryDark() {
 //        StatusBarCompatForKitKat.compat(this, getResources().getColor(R.color.colorPrimaryDark));
     }
+    /**
+     *返回键颜色
+     */
     protected void setNavigationColor(int backColor){
         this.backColor = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
         this.backColor.setColorFilter(getResources().getColor(backColor), PorterDuff.Mode.SRC_ATOP);
