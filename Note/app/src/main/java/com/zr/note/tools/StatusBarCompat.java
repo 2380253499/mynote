cfcf5620408811a678f99e95f6d18dd6696cddab
zhongrui <2380253499@qq.com>
2016-10-28 11:40:14 +0800

    状态栏样式更改

--- Note/app/src/main/java/com/zr/note/tools/StatusBarCompat.java
+++ Note/app/src/main/java/com/zr/note/tools/StatusBarCompat.java
@@ -7,38 +7,33 @@ import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 
 /**
- * Created by Administrator on 2016/8/1.
+ * Created by Administrator on 2016/10/28.
  */
 public class StatusBarCompat {
     private static final int INVALID_VAL = -1;
     private static final int COLOR_DEFAULT = Color.parseColor("#20000000");
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public static void compat(Activity activity, int statusColor) {
+    public static void compat(Activity activity, int statusColor)
+    {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (statusColor != INVALID_VAL) {
-                Window window = activity.getWindow();
-
-                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {//兼容5.0
-                    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                } else {
-                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);//兼容含5.1以上
-                }
-                window.setStatusBarColor(Color.TRANSPARENT);
-                window.setNavigationBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (statusColor != INVALID_VAL)
+            {
+                activity.getWindow().setStatusBarColor(statusColor);
             }
             return;
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        {
             int color = COLOR_DEFAULT;
             ViewGroup contentView = (ViewGroup) activity.findViewById(android.R.id.content);
-            if (statusColor != INVALID_VAL) {
+            if (statusColor != INVALID_VAL)
+            {
                 color = statusColor;
             }
             View statusBarView = new View(activity);
@@ -50,15 +45,18 @@ public class StatusBarCompat {
 
     }
 
-    public static void compat(Activity activity) {
+    public static void compat(Activity activity)
+    {
         compat(activity, INVALID_VAL);
     }
 
 
-    public static int getStatusBarHeight(Context context) {
+    public static int getStatusBarHeight(Context context)
+    {
         int result = 0;
         int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
+        if (resourceId > 0)
+        {
             result = context.getResources().getDimensionPixelSize(resourceId);
         }
         return result;
