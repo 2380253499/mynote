c335bdbd804fd7f35c4676f9645781d52c981955
zhongrui <2380253499@qq.com>
2016-11-10 17:09:28 +0800

    RecyclerView划分割线工具类

--- Note/app/src/main/java/com/zr/note/tools/PhoneUtils.java
+++ Note/app/src/main/java/com/zr/note/tools/PhoneUtils.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
+import android.net.Uri;
+import android.os.Build;
 
 /**
  * Created by Administrator on 2016/9/6.
@@ -40,5 +43,23 @@ public class PhoneUtils {
             return cm.getPrimaryClip().getItemAt(0).getText().toString();
         }
         return null;
+    }
+
+    /**
+     * 跳转到手机系统权限管理界面
+     * @param context
+     */
+    public static void PermissionsSetting(Context context) {
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            localIntent.setData(Uri.fromParts("package", context.getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            localIntent.setAction(Intent.ACTION_VIEW);
+            localIntent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails");
+            localIntent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+        }
+        context.startActivity(localIntent);
     }
 }
