0afb347f02000287afc16478e74c15fdbc72b5c4
zhongrui <2380253499@qq.com>
2016-12-09 17:20:56 +0800

    添加混淆的规则

--- Note/app/src/main/java/com/zr/note/base/MyApplication.java
+++ Note/app/src/main/java/com/zr/note/base/MyApplication.java
@@ -2,8 +2,6 @@ package com.zr.note.base;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 /**
  * Created by Administrator on 2016/12/8.
  */
@@ -11,9 +9,9 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
+        /*if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
-        LeakCanary.install(this);
+        LeakCanary.install(this);*/
     }
 }
