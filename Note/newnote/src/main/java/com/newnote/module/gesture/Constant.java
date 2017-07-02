7a80c2d6384ac3488027854a7003e1cefefe5382
zhongrui_home <1394572279@qq.com>
2017-07-02 20:47:01 +0800

    项目文件

---
+++ Note/newnote/src/main/java/com/newnote/module/gesture/Constant.java
@@ -0,0 +1,7 @@
+package com.newnote.module.gesture;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+public class Constant {
+}
---
+++ Note/newnote/src/main/java/com/newnote/module/home/Constant.java
@@ -0,0 +1,7 @@
+package com.newnote.module.home;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+public class Constant {
+}
