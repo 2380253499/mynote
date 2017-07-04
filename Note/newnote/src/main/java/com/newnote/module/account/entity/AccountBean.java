8137b084fb1931f259a803486d9d47507bd2441e
Administrator <2380253499@qq.com>
2017-07-04 09:39:57 +0800

    项目文件

---
+++ Note/newnote/src/main/java/com/newnote/module/account/entity/AccountBean.java
@@ -0,0 +1,56 @@
+package com.newnote.module.account.entity;
+
+
+import com.newnote.base.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class AccountBean extends BaseEntity implements Serializable{
+    /*dataSource TEXT,
+dataAccount TEXT,
+dataPassword TEXT,
+dataRemark TEXT,
+updateTime TimeStamp
+creatTime TimeStamp */
+    private String dataSource;
+    private String dataAccount;
+    private String dataPassword;
+    private String dataRemark;
+
+
+    public String getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(String dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public String getDataAccount() {
+        return dataAccount;
+    }
+
+    public void setDataAccount(String dataAccount) {
+        this.dataAccount = dataAccount;
+    }
+
+    public String getDataPassword() {
+        return dataPassword;
+    }
+
+    public void setDataPassword(String dataPassword) {
+        this.dataPassword = dataPassword;
+    }
+
+    public String getDataRemark() {
+        return dataRemark;
+    }
+
+    public void setDataRemark(String dataRemark) {
+        this.dataRemark = dataRemark;
+    }
+
+}
---
+++ Note/newnote/src/main/java/com/newnote/module/joke/entity/JokeBean.java
@@ -0,0 +1,29 @@
+package com.newnote.module.joke.entity;
+
+
+import com.newnote.base.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class JokeBean  extends BaseEntity implements Serializable{
+    private String dataContent;
+    private String dataRemark;
+    public String getDataContent() {
+        return dataContent;
+    }
+
+    public String getDataRemark() {
+        return dataRemark;
+    }
+
+    public void setDataRemark(String dataRemark) {
+        this.dataRemark = dataRemark;
+    }
+
+    public void setDataContent(String dataContent) {
+        this.dataContent = dataContent;
+    }
+}
