919e548b8f168e2d27c2208a45f9c085bd88946e
zhongrui <2380253499@qq.com>
2017-08-30 18:37:29 +0800

    newnote文件

---
+++ Note/newnote/src/main/java/com/newnote/module/home/contract/MainCon.java
@@ -0,0 +1,17 @@
+package com.newnote.module.home.contract;
+
+import com.github.baseclass.BasePresenter;
+import com.github.baseclass.BaseView;
+
+/**
+ * Created by administartor on 2017/8/30.
+ */
+
+public interface MainCon {
+    interface View extends BaseView{
+
+    }
+    interface Presenter extends BasePresenter<View>{
+
+    }
+}
---
+++ Note/newnote/src/main/java/com/newnote/module/home/contract/imp/MainImp.java
@@ -0,0 +1,8 @@
+package com.newnote.module.home.contract.imp;
+
+/**
+ * Created by administartor on 2017/8/30.
+ */
+
+public class MainImp {
+}
