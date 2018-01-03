035a1a0f0dce0dc9773345872f5aef30d25e9927
Administrator <2380253499@qq.com>
2018-01-03 14:23:45 +0800

    newnote文件

--- Note/customview/src/main/java/com/fast/utils/FileUtils.java
+++ Note/customview/src/main/java/com/fast/utils/FileUtils.java
@@ -2,6 +2,7 @@ package com.fast.utils;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.text.TextUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -128,6 +129,32 @@ public class FileUtils {
             fos.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void deleteFolderFile(String filePath, boolean deleteThisPath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            try {
+                File file = new File(filePath);
+                if (file.isDirectory()) { //目录
+                    File files[] = file.listFiles();
+                    for (int i = 0; i < files.length; i++) {
+                        deleteFolderFile(files[i].getAbsolutePath(), true);
+                    }
+                }
+                if (deleteThisPath) {
+                    if (!file.isDirectory()) { //如果是文件，删除
+                        file.delete();
+                    } else { //目录
+                        if (file.listFiles().length == 0) { //目录下没有文件或者目录，删除
+                            file.delete();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
